6678497c0a0b0b98b6353207d5ab7de122b9d7c8
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-01 00:25:32 -0700

    first unit test

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 flask-paranoid
 ==============
 
+[![Build Status](https://travis-ci.org/miguelgrinberg/flask-paranoid.svg?branch=master)](https://travis-ci.org/miguelgrinberg/flask-paranoid)
+
 Simple user session protection.
--- tests/test_paranoid.py
+++ tests/test_paranoid.py
@@ -1,8 +1,25 @@
 import unittest
+from flask import Flask
 from flask_paranoid import Paranoid
-
-Paranoid()
 
 
 class ParanoidTests(unittest.TestCase):
-    pass
+    def test_simple(self):
+        app = Flask(__name__)
+        app.config['SECRET_KEY'] = 'foo'
+        Paranoid(app)
+
+        @app.route('/')
+        def index():
+            return 'foobar'
+
+        client = app.test_client(use_cookies=True)
+
+        rv = client.get('/', headers={'User-Agent': 'foo'})
+        self.assertEqual(rv.status_code, 200)
+
+        rv = client.get('/', headers={'User-Agent': 'foo'})
+        self.assertEqual(rv.status_code, 200)
+
+        rv = client.get('/', headers={'User-Agent': 'bar'})
+        self.assertEqual(rv.status_code, 401)
