5ff4166a8c9ab167ff39118ecaf735dc66952f35
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-01 19:32:08 -0700

    Release 0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Paranoid',
-    version='0.0.1',
+    version='0.1.0',
     url='http://github.com/miguelgrinberg/flask-paranoid/',
     license='MIT',
     author='Miguel Grinberg',
